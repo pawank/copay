6e91eea31b88ce11ecc66ccdd489101fb5bd0465
Pawan Kumar <pawan@acelrtech.com>
2020-04-18 23:02:21 +0530

    Move apk file

--- ui/make_build.sh
+++ ui/make_build.sh
@@ -5,3 +5,4 @@ echo "Starting release builds.."
 #flutter build apk --debug
 flutter build appbundle
 flutter build apk --split-per-abi
+cp build/app/outputs/apk/release/app-armeabi-v7a-release.apk ~/Downloads/app.apk
